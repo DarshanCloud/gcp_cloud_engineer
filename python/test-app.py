131cbf92c83084cd64bb6fb8f800b4818192a94f
Darshan S <120465084+DarshanCloud@users.noreply.github.com>
2024-07-30 15:36:50 +0530

    Update test-app.py

--- python/test-app.py
+++ python/test-app.py
@@ -1,13 +1,13 @@
 import os
 import pytest
 
-import main
+import app
 
 
 @pytest.fixture
 def client():
-    main.app.testing = True
-    return main.app.test_client()
+    app.app.testing = True
+    return app.app.test_client()
 
 
 def test_handler_no_env_variable(client):
