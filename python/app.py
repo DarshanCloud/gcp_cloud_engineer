24af9ab045c4d82ddb298f8688cdf391148df482
Darshan S <120465084+DarshanCloud@users.noreply.github.com>
2024-07-30 15:44:28 +0530

    add

--- python/app.py
+++ python/app.py
@@ -1,14 +1,16 @@
-import os
+# import os
 
-from flask import Flask
+# from flask import Flask
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
 
-@app.route("/")
-def hello_world():
-    name = os.environ.get("NAME", "World")
-    return f"Hello {name}!"
+# @app.route("/")
+# def hello_world():
+#     name = os.environ.get("NAME", "World")
+#     return f"Hello {name}!"
 
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+# if __name__ == "__main__":
+#     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+
+
